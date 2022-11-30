045e3d199d7d33ef933e7c5884cac8db786d1424
Benedict <benedictryan80@gmail.com>
2022-11-30 14:05:15 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: false
+};
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
